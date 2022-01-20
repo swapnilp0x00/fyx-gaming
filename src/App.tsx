76fd795e3ec9c4b1056208a2d52366cdf6e0fa50
swapnil <ddevil691@gmail.com>
2022-01-20 02:30:15 -0500

    added main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Main from './components/Main';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Main></Main>
     </div>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      I am Main
+    </div>
+  );
+}
+
+export default Main;
